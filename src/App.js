6787e41966c28758b27f42c7d3b519d13bc3ba2d
waqasnaeem <waqasnaeem91@gmail.com>
2022-10-16 04:03:29 +0500

    configured-firebase-push-notification-functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect,useState } from 'react';
 import {  useSelector } from 'react-redux';
 
 // slices
@@ -7,13 +8,26 @@ import Router from './routes';
 // theme
 import ThemeProvider from './theme';
 // components
+import { getPermission, onMessageListener } from './firebase';
 import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 
 // ----------------------------------------------------------------------
 
 export default function App() {
+  const [isTokenFound, setTokenFound] = useState(false);
   const isAuth = useSelector(getAuth);
+  
+  useEffect(() => {
+    getPermission(setTokenFound);
+    console.log(isTokenFound)
+    onMessageListener().then(payload => {
+      // setShow(true);
+      // setNotification({title: payload.notification.title, body: payload.notification.body})
+      console.log("payload",payload);
+    }).catch(err => console.log('failed: ', err));
+  },[])
+  
   return (
     <ThemeProvider>
         <ScrollToTop />
